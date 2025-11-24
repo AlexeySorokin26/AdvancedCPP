52a628f55b5e071ab99692a7c059f9defa3fb7e5
Alexey Sorokin <asorokin@gremse-it.com>
2025-11-24 23:58:22 +0100

    first

---
+++ part1/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Tmp {
+	char d; // 1
+	int a; // 4
+	double c; // 8
+};
+
+
+void f(int) {}
+void f(double) {}
+
+int main() {
+	void (*p)(int) = &f;
+}
