1a0a1d5056ecbc7dc231534882efb6334487daac
Alexey Sorokin <asorokin@gremse-it.com>
2025-11-26 00:14:15 +0100

    update

--- part1/main.cpp
+++ part1/main.cpp
@@ -2,16 +2,13 @@
 #include <vector>
 #include <algorithm>
 
-class Tmp {
-	char d; // 1
-	int a; // 4
-	double c; // 8
-};
-
-
-void f(int) {}
-void f(double) {}
+int& g() {
+	int* p = new int(1);
+	return *p;
+}
 
 int main() {
-	void (*p)(int) = &f;
+	int x = 0;
+	int* p = &x;
+	int*& ref = p;
 }
