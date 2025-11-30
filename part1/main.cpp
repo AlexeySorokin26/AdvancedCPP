269009d2d733b83dc281f65c4eeb28f53501bd2d
Alexey Sorokin <asorokin@gremse-it.com>
2025-11-30 23:39:21 +0100

    update

--- part1/main.cpp
+++ part1/main.cpp
@@ -2,22 +2,10 @@
 #include <vector>
 #include <algorithm>
 
-struct S {
-private:
-	int a;    // 4 
-	double b; // 8
-public:
-	int GetA() const { return a; }
-	double GetB() const { return b; }
-};
 
 int main(int argc, char* argv[]) {
-	S s;
-
-	reinterpret_cast<int&>(s) = 12;
-	std::cout << s.GetA() << std::endl;
-
-	char* tmp = reinterpret_cast<char*>(&s) + 8;
-	reinterpret_cast<double&>(*tmp) = 34.56;
-	std::cout << s.GetB() << std::endl;
+	int x = 10;
+	const double& y = x;
+	x = 100;
+	std::cout << y << std::endl;
 }
