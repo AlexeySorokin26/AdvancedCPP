4f84811cd2cee21ff400c021048241c9d476d94d
Alexey Sorokin <asorokin@gremse-it.com>
2026-01-02 21:23:15 +0300

    vtable

--- part1/main.cpp
+++ part1/main.cpp
@@ -2,13 +2,23 @@
 #include <vector>
 #include <algorithm>
 
+struct G {
+	int g;
+};
 
-void f(int a) {
+struct D : virtual G {
+	int d;
+};
 
-}
+struct M : virtual G {
+	int m;
+};
 
+struct S : M, D {
+	int s;
+};
 
 int main(int argc, char* argv[]) {
-
-	
+	S s;
+	std::cout << s.g << std::endl;
 }
